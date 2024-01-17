7e9210eb513fdf20ce9fe0d96970da3252807784
Mario Bezerra <mariomgsb@gmail.com>
2024-01-17 17:22:24 -0300

    Base Event and Events for Commands created

---
+++ SM-Post/Post.Common/Events/CommentRemovedEvent.cs
@@ -0,0 +1,11 @@
+using CQRS.Core.Events;
+
+namespace Post.Common.Events {
+    public class CommentRemovedEvent : BaseEvent {
+        public CommentRemovedEvent(string type) : base(nameof(CommentRemovedEvent)) {
+
+        }
+
+        public Guid CommentId { get; set; }
+    }
+}
---
+++ SM-Post/Post.Common/Events/PostCreatedEvent.cs
@@ -0,0 +1,13 @@
+using CQRS.Core.Events;
+
+namespace Post.Common.Events {
+    public class PostCreatedEvent : BaseEvent {
+        public PostCreatedEvent(string type) : base(nameof(PostCreatedEvent)) {
+
+        }
+
+        public string Author { get; set; }
+        public string Message { get; set; }
+        public DateTime DatePosted { get; set;}
+    }
+}
